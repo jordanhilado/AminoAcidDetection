01a2b7455246115f2f27f4cfe83f96c676520939
Jordan Hilado <jordanalihilado@gmail.com>
2022-04-27 16:32:07 -0700

    update gitignore

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from flask import Flask, render_template, request
 import sqlite3 as sql
 from Bio.Seq import Seq
@@ -11,20 +12,27 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-        return render_template("index.html")
-    else:
-        sequence = request.form.get("sequence")
-        translatedSeq = "AUGT"
-        with sql.connect("database.db") as con:
+    if request.method == 'POST':
+        if request.form == "Reset":
+            return render_template("dna.html")
+        try:
+            sequence = request.form.get("sequence")
+            translatedSeq = "AUGT"
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO results (oldsequence, newsequence) VALUES (?,?)",(sequence, translatedSeq))
+                con.commit()
+        except:
+            con.rollback()
+        finally:
+            con = sql.connect("database.db")
+            con.row_factory = sql.Row
             cur = con.cursor()
-            cur.execute("INSERT INTO results (oldsequence, newsequence) VALUES (?, ?)", (sequence, translatedSeq))
-            con.commit()
-        con.row_factory = sql.Row
-        cur.execute("select * from results")
-        rows = cur.fetchall();
-        print(rows)
-        return render_template("index.html", rows=rows)
+            cur.execute("select * from results")
+            rows = cur.fetchall();
+            return render_template("index.html", rows = rows)
+    else:
+        return render_template("index.html")
 
 @app.route("/dna")
 def dna():
