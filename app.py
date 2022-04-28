2710dd0b1e25d2a32b92dfa76dd8df09d6c9f459
Jordan Hilado <jordanalihilado@gmail.com>
2022-04-27 19:15:11 -0700

    working on clear table function

--- app.py
+++ app.py
@@ -13,24 +13,27 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        if request.form == "Reset":
-            return render_template("dna.html")
-        try:
-            sequence = request.form.get("sequence")
-            translatedSeq = "AUGT"
-            with sql.connect("database.db") as con:
+        if request.form.type == 'reset':
+            conn.execute('DELETE FROM results')
+            conn.commit()
+            return render_template('index.html')
+        else:
+            try:
+                sequence = request.form.get("sequence")
+                translatedSeq = "AUGT"
+                with sql.connect("database.db") as con:
+                    cur = con.cursor()
+                    cur.execute("INSERT INTO results (oldsequence, newsequence) VALUES (?,?)",(sequence, translatedSeq))
+                    con.commit()
+            except:
+                con.rollback()
+            finally:
+                con = sql.connect("database.db")
+                con.row_factory = sql.Row
                 cur = con.cursor()
-                cur.execute("INSERT INTO results (oldsequence, newsequence) VALUES (?,?)",(sequence, translatedSeq))
-                con.commit()
-        except:
-            con.rollback()
-        finally:
-            con = sql.connect("database.db")
-            con.row_factory = sql.Row
-            cur = con.cursor()
-            cur.execute("select * from results")
-            rows = cur.fetchall();
-            return render_template("index.html", rows = rows)
+                cur.execute("select * from results")
+                rows = cur.fetchall();
+                return render_template("index.html", rows = rows)
     else:
         return render_template("index.html")
 
